40803a7b3d3d13a787f46a13f26bf1a7c7a1af89
RohiniMane007 <manerohini295@gmail.com>
2024-11-19 16:38:31 +0530

    ui

--- lib/features/expense/view/screen/expense_view.dart
+++ lib/features/expense/view/screen/expense_view.dart
@@ -16,7 +16,7 @@ class ExpenseView extends StatelessWidget {
             height: 250,
           ),
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               ElevatedButton(onPressed: () {}, child: const Text("Week")),
               ElevatedButton(onPressed: () {}, child: const Text("Month")),
@@ -30,7 +30,70 @@ class ExpenseView extends StatelessWidget {
             child: ListView.builder(
               itemCount: 2,
               itemBuilder: (context, index) {
-                return const Text("data");
+                return Card(
+                  elevation: 3,
+                  shadowColor: Colors.orange,
+                  color: Colors.white,
+                  margin: const EdgeInsets.only(right: 10, left: 10),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  child: SizedBox(
+                    height: 100,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(left: 10),
+                          child: SizedBox(
+                            width: 35,
+                            height: 35,
+                            child: Icon(Icons.home),
+                          ),
+                        ),
+                        const Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.only(top: 20),
+                              child: Text(
+                                "category",
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(bottom: 20),
+                              child: SizedBox(
+                                width: 50,
+                                child: Text(
+                                  "description",
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                        const Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Text(
+                              " ₹ data",
+                            ),
+                            Text(
+                              "sub",
+                            )
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            IconButton(
+                                onPressed: () async {},
+                                icon: const Icon(Icons.edit)),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                );
               },
             ),
           )
