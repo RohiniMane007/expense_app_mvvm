71729d72545b01950e801dee9715e1235a3f3d11
RohiniMane007 <manerohini295@gmail.com>
2024-11-30 15:51:31 +0530

    change

--- lib/comman/bloc/cubit/appbar_state.dart
+++
@@ -1,9 +0,0 @@
-part of 'appbar_cubit.dart';
-
-class AppbarState extends Equatable {
-  final String username;
-  const AppbarState({this.username = ""});
-
-  @override
-  List<Object> get props => [username];
-}
--- lib/main.dart
+++ lib/main.dart
@@ -14,24 +14,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: BlocProvider(
-        create: (context) => AuthBloc(),
-        // providers: [
-        //   BlocProvider<AuthBloc>(
-        //     create: (BuildContext context) => AuthBloc(),
-        //   ),
-        //   BlocProvider<AppbarCubit>(
-        //     create: (BuildContext context) => AppbarCubit(),
-        //   ),
-        // ],
-        child: const LoginView(),
-      ),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const MyApp()
+        // home: BlocProvider(
+        //   create: (context) => AuthBloc(),
+        //   child: const LoginView(),
+        // ),
+        );
   }
 }
