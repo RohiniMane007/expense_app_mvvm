e5c28eb6ee585e588c5af372e9355138bdc06d2e
RohiniMane007 <manerohini295@gmail.com>
2024-12-01 17:23:09 +0530

    change

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 // import 'package:expense_app/datepicker.dart';
-import 'package:expense_app/features/auth/view/screen/login_view.dart';
-import 'package:expense_app/features/auth/view_model/bloc/auth_bloc.dart';
+// import 'package:expense_app/features/auth/view/screen/login_view.dart';
+// import 'package:expense_app/features/auth/view_model/bloc/auth_bloc.dart';
+import 'package:expense_app/routes/route_name.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'routes/routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,11 +24,12 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      // home: const CustomDate()
-      home: BlocProvider(
-        create: (context) => AuthBloc(),
-        child: const LoginView(),
-      ),
+      // home: BlocProvider(
+      //   create: (context) => AuthBloc(),
+      //   child: const LoginView(),
+      // ),
+      initialRoute: RouteName.login,
+      onGenerateRoute: Routes.generatedRoute,
     );
   }
 }
